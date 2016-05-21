11084995f4b563888d3c780d11071aa6b246d731
RitterVomNi <heinen.m@web.de>
2016-05-21 23:31:53 +0200

    implemented encrypted message sending

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,4 +3,22 @@ class MessagesController < ApplicationController
     @user = User.find_by(login: params[:login])
     render json: @user.messages
   end
+
+  def create
+    recipient = User.find_by(login: params[:recipient]).id
+    content_enc = Base64.encode64(params[:content_enc])
+    sig_recipient = Base64.encode64(params[:sig_recipient])
+    sig_service = Base64.encode64(params[:sig_service])
+    key_recipient_enc = Base64.encode64(params[:key_recipient_enc])
+
+    msg = Message.new(recipient: recipient, content_enc: content_enc, sender: params[:sender],
+                      iv: params[:iv], key_recipient_enc: key_recipient_enc, sig_recipient: sig_recipient,
+                      sig_service: sig_service)
+
+    if msg.save
+      render nothing: true , status: 200
+    else
+      render nothing: true , status: 404
+    end
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   def index
-    render :json => User.all
+    render :json => User.all.to_json(only: %w(salt_masterkey login privkey_user_enc))
   end
 
   def anmelden
@@ -8,7 +8,7 @@ class UsersController < ApplicationController
     if @user.nil?
       render nothing: true , status: 404
     else
-    render json: @user.to_json(only: %w(login salt_masterkey pubkey_user privkey_user_enc))
+    render json: @user.to_json(only: %w(salt_masterkey privkey_user_enc pubkey_user))
     end
   end
 
@@ -22,10 +22,10 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(login: params[:login])
+    @user = User.new(login: params[:login], salt_masterkey: params[:salt_masterkey], pubkey_user: params[:pubkey_user], privkey_user_enc: params[:privkey_user_enc])
 
       if @user.save
-        render json: @user
+        render json: @user.to_json(only: %w(login))
       else
         render nothing: true , status: 404
       end
