c98e3249d034004e5b170d11bb8bbc3732076dd7
RitterVomNi <heinen.m@web.de>
2016-05-28 13:31:28 +0200

    failsafe default anmelden

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,14 +3,17 @@ class UsersController < ApplicationController
   def anmelden
     @user = User.find_by(login: params[:login])
 
-    head 400 unless @user != nil
-    
-    render json: @user.to_json(only: %w(salt_masterkey privkey_user_enc pubkey_user))
-
+    if @user.nil?
+      # Return status 400
+      head 400
+    else
+      render json: @user.to_json(only: %w(salt_masterkey privkey_user_enc pubkey_user))
+    end
   end
 
   def pubkey
     @user = User.find_by(login: params[:login])
+
     if @user.nil?
       head 404
     else
@@ -22,17 +25,20 @@ class UsersController < ApplicationController
     @user = User.new(login: params[:login], salt_masterkey: params[:salt_masterkey], pubkey_user: params[:pubkey_user], privkey_user_enc: params[:privkey_user_enc])
 
       if @user.save
-        render nothing: true , status: 201
+        head 201
       else
-        render nothing: true , status: 400
+        head 400
       end
   end
 
   def destroy
+
     user = User.find_by(login: params[:login])
+
     user.destroy
 
-    render nothing: true ,  status: 200
+    head 200
+
   end
 
 end
