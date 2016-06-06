8551d4a062aebdce63a031fb01fcc1d0bc3e3e28
RitterVomNi <heinen.m@web.de>
2016-06-06 13:36:57 +0200

    rebuild destroy user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -33,7 +33,7 @@ class UsersController < ApplicationController
 
   def destroy
 
-    user = User.check_sig(params[:timestamp], params[:login], params[:digitale_signatur])
+    user = User.check_sig(params[:timestamp].to_s, params[:login], params[:digitale_signatur])
 
     user.destroy
 
