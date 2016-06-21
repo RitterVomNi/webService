42bcf862e9ab79c152aceac1783bb72c29189b8c
RitterVomNi <heinen.m@web.de>
2016-06-21 17:06:01 +0200

    added user attr validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ActiveRecord::Base
   has_many :messages, dependent: :destroy, foreign_key: 'recipient'
-  validates :login, uniqueness: true
+  validates :login, uniqueness: true, presence: true
+  validates :salt_masterkey, presence: true
+  validates :pubkey_user, presence: true
+  validates :privkey_user_enc, presence: true
 
 
   # Überprüft die digitale Signatur mit dem pubkey des Senders
